f71b401e37c91647e214fd535d46bfb9e4c87274
Ol-create <oluola96@gmail.com>
2022-05-21 17:47:42 -0600

    feat: create alanKey

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import react from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const alankey = '330f418252034aa6759dadb69aa7e1732e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+	useEffect(() => {
+		alanBtn({
+             KEY: ALAkEY,
+		}, [])
+	})
 	return (
 		<div>
 			<h1>Alan AI News Application</h1>
